86cc5a805d1bd2e291baae0f9d4a6d5d40e3ea67
gierczym <michal.gierczynski@pw.edu.pl>
2024-11-24 09:57:19 +0100

    tmp function add to line numbers vector

--- dynamicword.c
+++ dynamicword.c
@@ -26,11 +26,27 @@ dynamic_word_t init_dynamic_word( char *word_string, int n_max ) {
 	dynamic_word->n_max = n_max;
 	dynamic_word->line_numbers_vect = malloc( n_max * sizeof *dynamic_word->line_numbers_vect );
 	if( NULL == dynamic_word->line_numbers_vect ) {
-		fprintf( stderr, "init_dynamic_word: [!] nie udalo sie zaalokowac pamieci na typ line_numbers_vect\n" );
+		fprintf( stderr, "init_dynamic_word: [!] nie udalo sie zaalokowac pamieci na line_numbers_vect\n" );
 		return NULL;
 	}
 
 	return dynamic_word;
 }
 
+int add_to_dynamic_word( dynamic_word_t word_in, int line_number ) {
+	if ( word_in->n_curr == word_in->n_max ) {
+		int *tmp = realloc( word_in->line_numbers_vect * sizeof *word_in->line_numbers_vect );
+		if( NULL == tmp ) {
+			fprintf( stderr, "add_to_dynamic_word: [!] nie udalo sie zaalokowac pamieci na line_numbers_vect\n" );
+			return 1;
+		}
+		word_in->line_numbers_vect
+	} else {
+		word_in->line_numbers_vect[word_in->n_curr] = line_number;
+		word_in->n_curr++;
+	}
+	
+	return 0;
+	
+}
 
