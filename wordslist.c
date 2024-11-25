b7691ed7b9aaccddebe6e3eb7acca82d1f2509b1
gierczym <michal.gierczynski@pw.edu.pl>
2024-11-25 19:31:10 +0100

    Module:  wordslist, bugfixed

--- wordslist.c
+++ wordslist.c
@@ -47,7 +47,10 @@ int add_to_words_list( words_list_t list, char *word ) {
 			return EXIT_FAILURE;
 		}
 		list->words[list->n_curr] = tmpword;
-		strcpy( list->words[list->n_curr], word );
+		int i;
+		for( i = 0; i <= strlen(word); i++ ) {
+			*(list->words[list->n_curr]+i) = *(word+i);
+		}
 		list->n_curr++;
 	} else {
 		char *tmpword = malloc( (strlen(word)+1) * sizeof(char) );
@@ -56,7 +59,10 @@ int add_to_words_list( words_list_t list, char *word ) {
 			return EXIT_FAILURE;
 		}
 		list->words[list->n_curr] = tmpword;
-		strcpy( list->words[list->n_curr], word );
+		int i;
+		for( i = 0; i <= strlen(word); i++ ) {
+			*(list->words[list->n_curr]+i) = *(word+i);
+		}
 		list->n_curr++;
 	}
 	return 0;
@@ -68,18 +74,21 @@ int fill_words_list( words_list_t list, FILE *in ) {
 	char *word;
 	
 	int lines = 0;
+	int new_word;
 	while( fgets(buf, BUFSIZE, in) != NULL ) {
 			lines++;
 			word = find_word( buf );
 			if( list->n_curr == 0) {
-					printf( "adding word: %s\n", word );
 					add_to_words_list( list, word );
 			}
 			while( word != NULL ) {
+				new_word = 1;
 				for( i = 0; i < list->n_curr; i++ ) {
-					if( strcmp(word, list->words[i]) != 0 )
-						printf( "adding word: %s\n", word );
-						add_to_words_list( list, word );
+					if( strcmp(word, list->words[i]) == 0 )
+						new_word = 0;
+				}
+				if( new_word == 1) {
+					add_to_words_list( list, word );
 				}
 				word = find_word( NULL );
 			}
